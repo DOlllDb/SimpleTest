aa44cc541febb27d6d69a5c1d29912aa1ee9920a
Maksim Nikelman <jemani@yandex.ru>
2018-02-07 00:04:16 +0300

    fix

--- src/test/java/resources/FailedTest.java
+++ src/test/java/resources/FailedTest.java
@@ -1,16 +1,12 @@
+package resources;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
  * Created by jeman on 06.02.18.
  */
-public class SimpleTest {
-
-    @Test
-    public void successTest() {
-        System.out.println("Success test");
-    }
-
+public class FailedTest {
     @Test
     public void failedTest() {
         System.out.println("Failed test");
---
+++ src/test/java/resources/SuccessTest.java
@@ -0,0 +1,15 @@
+package resources;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by jeman on 06.02.18.
+ */
+public class SuccessTest {
+
+    @Test
+    public void successTest() {
+        System.out.println("Success test");
+    }
+}
